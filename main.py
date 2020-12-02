2ec8786e2dcfb545bc650d6f07f1e072d49cac44
Nivas Muthu M G <nivasmuthu452@gmail.com>
2020-12-02 18:32:08 +0530

    Basic Implementation: menu(): used to display the CLI UI config(): gets the data for database connection from database.ini connect(): creates a database object and returns it push_data(): pushes the collection into table get_password(): gets the user password save_new_record(): gets all the info for the record
    Ability to save a new record to the database
    Ability to exit the program

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # /usr/bin/python3
 from configparser import ConfigParser
+from os import name
 
 import psycopg2
 
@@ -22,21 +23,67 @@ def config(filename='database.ini', section='postgresql'):
 
 def connect():
     connection = None
+
     try:
         params = config()
         connection = psycopg2.connect(**params)
-
         cursor = connection.cursor()
-
         cursor.execute("SELECT version()")
-
         print(cursor.fetchone())
+
     except(Exception, psycopg2.DatabaseError) as error:
         print(error)
-    finally:
-        if connection != None:
-            connection.close()
-            print("Closing Connection")
+
+    return connection
 
 
-connect()
+def push_data(collection: dict):
+    connection = connect()
+    if connection != None:
+        cursor = connection.cursor()
+        cursor.execute("INSERT INTO password_collection(name, url, username, password) VALUES('{name}', '{url}', '{username}', '{password}')".
+                       format(name=collection["name"], url=collection["url"], username=collection["username"], password=collection["password"]))
+
+        connection.commit()
+        connection.close()
+
+
+def get_password(choice: str):
+    if choice == "Y":
+        return input("Enter the password:")
+
+
+def save_new_record():
+    collection = {}
+    collection["name"] = input("Enter the Name: ")
+    collection["url"] = input("URL: ")
+    collection["username"] = input("Username: ")
+    password = ''
+    while True:
+        print("Do you have your own password, else we will generate a strong password")
+        print("Y/N")
+        choice = input(":").upper()
+        if choice == "Y" or choice == "N":
+            collection["password"] = get_password(choice)
+            break
+        else:
+            print("Wrong Option, try again")
+    push_data(collection)
+
+
+def menu():
+    while True:
+        print("------------------Cero Password Manager------------------")
+        print("Select a option by entering it's numeric")
+        print("1. Save a New Password")
+        print("0. Exit")
+        choice = input(":")
+        if choice == "1":
+            save_new_record()
+        elif choice == "0":
+            return
+        else:
+            print("Wrong Option, Try again")
+
+
+menu()
